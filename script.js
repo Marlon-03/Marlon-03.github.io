6a8a8b4748f23dd88d01352192dbf3118435e63d
Marlon-03 <marlonmovilla03@gmail.com>
2024-02-20 16:58:48 +0800

    skill

--- script.js
+++ script.js
@@ -52,3 +52,12 @@ function changeTab(index) {
 }
 
 changeTab(0);
+
+
+
+// trial
+const checkbox = document.getElementById('checkbox');
+checkbox.addEventListener('change', () => {
+    // Change the theme of the website
+    document.body.classList.toggle('dark-theme');
+});
